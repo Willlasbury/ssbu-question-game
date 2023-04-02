71362f59f09495063c8ec016a2011e6ff7d65c67
Willlasbury <asburyw229@gmail.com>
2023-04-02 14:46:51 -0700

    got value to return from form

--- pages/highscore/js/script.js
+++ pages/highscore/js/script.js
@@ -1,1 +1,15 @@
-console.log("test")
+function getVal(selector){
+    let form = document.forms[0]
+    console.log("form:", form)
+    // let input = form.querySelector(`input=name["${selector}"]`)
+    var input = document.getElementById('first-initial').value;
+    console.log("input:", input)
+    return input
+}
+
+// let firstInput = getVal('input[name="first-initial"]')
+// console.log("firstInput:", firstInput)
+
+// let input = document.querySelector('input[name="#first-initial"]')
+
+// console.log("input:", input)
