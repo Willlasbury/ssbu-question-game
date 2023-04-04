ffb88f0cae71c0fd6de1ebbb9e621060d7d73e93
Willlasbury <asburyw229@gmail.com>
2023-04-03 20:08:25 -0700

    displays top five scores

--- pages/highscore/js/script.js
+++ pages/highscore/js/script.js
@@ -2,24 +2,52 @@
 let highScoreUl = document.querySelector("#highScore");
 
 function getUserScore() {
-  console.log("test")
   let highScores = localStorage.getItem("previousScores").split(",");
   // let userInit = highScores.slice(0, 3).join("").toUpperCase();
   // let userScore = highScores[3];
-  console.log("highScores:", highScores)
   displayScores(highScores);
 }
 
+let scoresToDisplay = [];
+let checkArray = [0, 0, 0, 0, 0];
+function findScoresToDisplay(array) {
+  for (let i = 0; i < array.length; i = i + 4) {
+
+    let group = array.slice(i, i + 4);
+    let checkScore = group[3];
+    Number(checkScore);
+
+    if (checkScore >= checkArray[0]) {
+      checkArray[4] = group[3];
+      checkArray.sort().reverse().slice(0, 6);
+      scoresToDisplay = scoresToDisplay.concat([group]);
+
+      scoresToDisplay
+        .sort(function (a, b) {
+          return a[3] - b[3];
+        })
+        .reverse();
+      // console.log("scoreToDisplay:", scoresToDisplay)
+      // scoresToDisplay.sort();
+      // console.log("checkArray:", checkArray)
+      scoresToDisplay = scoresToDisplay.slice(0, 5);
+    }
+
+    flatScores = scoresToDisplay.flat();
+    console.log("flatScores:", flatScores)
+  }
+  return flatScores;
+}
+
 function displayScores(array) {
-  let newLi = document.createElement("li");
-  for (let i = 0; i < array.length; i=i+4) {
-    console.log("test")
-    let group = array.slice(i,i+4)
-    let userInit = group.slice(0, 3).join("").toUpperCase();
-    let userScore = highScores[3];
+  console.log("array:", array)
+  for (let i = 0; i < array.length; i = i + 4) {
+    let anotherGroup = array.slice(i, i + 4);
+    let userInit = anotherGroup.slice(0, 3).join("").toUpperCase();
+    let userScore = anotherGroup[3];
+    let newLi = document.createElement("li");
     newLi.textContent = `${userInit}: ${userScore}`;
     highScoreUl.appendChild(newLi);
-    console.log("group:", group)
   }
 }
 
@@ -30,7 +58,6 @@ let score = localStorage.getItem("timeScore");
 // pull value from input field
 function getVal(selector) {
   var input = document.querySelector(`#${selector}`).value;
-  // console.log("input:", input)
   return input;
 }
 
@@ -47,9 +74,11 @@ function logInitials() {
   } else {
     let previousScores = localStorage.getItem("previousScores").split(",");
     highScores = previousScores.concat(initials, scores);
-    console.log("highscores2:", highScores);
     JSON.stringify(highScores);
     localStorage.setItem("previousScores", highScores);
   }
-  getUserScore()
+  getUserScore();
 }
+
+let highScores = localStorage.getItem("previousScores").split(",");
+displayScores(findScoresToDisplay(highScores));
